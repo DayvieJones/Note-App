2887e7c59d13c6729b1832fa0aec82afcaa177bf
David Johrden <david.johrden@gmail.com>
2023-12-24 10:47:58 +0100

    finished note app

--- script.js
+++ script.js
@@ -1,68 +1,118 @@
-document.addEventListener("DOMContentLoaded", loadCardsFromLocalStorage);
 const createNoteEl = document.querySelector(".createNote");
 const saveNoteEl = document.querySelector(".saveNote");
 const deleteNoteEl = document.querySelector(".deleteNote");
+const inputHeaderElement = document.querySelector("#inputHeader");
+const inputTextAreaElement = document.querySelector("#inputTextArea");
 
-createNoteEl.addEventListener("click", createNewNote);
-saveNoteEl.addEventListener("click", createNewCard);
+const LOCAL_STORAGE_NOTES = "noteApp-Notes";
+
+createNoteEl.addEventListener("click", clickNewNote);
+saveNoteEl.addEventListener("click", clickSaveButton);
 deleteNoteEl.addEventListener("click", deleteSelectedCard);
 
 let cardNotes = [];
 let currentSelectedCardID = null;
 
+loadCardsFromLocalStorage();
+
+function getNotes() {
+  return JSON.parse(localStorage.getItem(LOCAL_STORAGE_NOTES)) || [];
+}
+
 function loadCardsFromLocalStorage() {
-  //Methode um ein JSON String in Objekt/Array umzuwandeln
-  cardNotes = JSON.parse(localStorage.getItem("cardNotes")) || [];
+  //Method to exchange a JSON String into object/array
+  const cardNotes = getNotes();
+
   cardNotes.forEach((element) => {
     appendCard(element);
   });
 }
 
 function saveCardsToLocalStorage() {
-  localStorage.setItem("cardNotes", JSON.stringify(cardNotes));
-}
-
-//gets the input of header
-function getInputHeader() {
-  return document.querySelector("#inputHeader");
-}
-
-//gets the input of the Textarea
-function getInputTextArea() {
-  return document.querySelector("#inputTextArea");
+  localStorage.setItem(LOCAL_STORAGE_NOTES, JSON.stringify(cardNotes));
 }
 
 function getCurrentlySelectedNote() {
-  return document.querySelector(".selected-note");
+  return document.querySelector(".selectedNote");
 }
 
 //clear the input fields
-function createNewNote() {
-  getInputHeader().value = "";
-  getInputTextArea().value = "";
+function clickNewNote() {
+  inputHeaderElement.value = "";
+  inputTextAreaElement.value = "";
+
+  removeSelectedClassFromAllNotes();
+}
+
+function getNextId() {
+  const notes = getNotes();
+
+  const sortedNotes = notes.sort((noteA, noteB) => noteA.id - noteB.id);
+
+  let nextId = 1;
+
+  for (let note of sortedNotes) {
+    if (nextId < note.id) break;
+
+    nextId = note.id + 1;
+  }
+  return nextId;
 }
 
 //create a new card with values and checks input before append
-function createNewCard() {
-  const randomID = Math.floor(Math.random() * 1000000);
-  const timeStamp = new Date().toLocaleString("de-DE");
+function clickSaveButton() {
+  const header = inputHeaderElement.value;
+  const text = inputTextAreaElement.value;
 
-  //creating the Card Object
-  const newCard = {
-    id: randomID,
-    header: getInputHeader().value,
-    text: getInputTextArea().value,
-    dateStamp: timeStamp,
-  };
-
-  if (!newCard.header) {
+  if (!header) {
     alert("Please write text in the header area to safe the note");
-  } else if (!newCard.text) {
+    return;
+  } else if (!text) {
     alert("Please write text in the text area to safe the note");
-  } else {
+    return;
+  }
+  let currentId = undefined;
+
+  const currentlySelectedNoteEl = getCurrentlySelectedNote();
+
+  if (currentlySelectedNoteEl) currentId = currentlySelectedNoteEl.id;
+
+  saveCard(text, header, Number(currentId));
+
+  inputHeaderElement.value = "";
+  inputTextAreaElement.value = "";
+}
+
+function saveCard(text, header, id = undefined) {
+  console.log(id);
+  //creating the Card Object
+  if (!id) {
+    const newCard = {
+      id: getNextId(),
+      header,
+      text,
+      dateStamp: new Date().toLocaleString("de-DE"),
+    };
     cardNotes.push(newCard);
     appendCard(newCard);
+  } else {
+    const indexOfNoteWithId = cardNotes.findIndex((note) => note.id === id);
+
+    if (indexOfNoteWithId > -1) {
+      cardNotes[indexOfNoteWithId] = {
+        id,
+        header,
+        text,
+        dateStamp: new Date().toLocaleString("de-DE"),
+      };
+    }
+    getCurrentlySelectedNote().remove();
+    appendCard(cardNotes[indexOfNoteWithId]);
+
+    //
   }
+  saveCardsToLocalStorage();
+  clickNewNote();
 }
 
 //get a created card w/ values and append these into wrapper
@@ -91,35 +141,33 @@ function appendCard(newCard) {
   cardEl.appendChild(cardText);
   cardEl.appendChild(cardDate);
   document.querySelector(".cardWrapper").appendChild(cardEl);
-  saveCardsToLocalStorage();
-  createNewNote();
+
+  clickNewNote();
 }
 
 //select a card and shows the current content
 function selectCard(id) {
   const selectedCardEl = document.querySelector(`.noteCard[id="${id}"]`);
+
   if (selectedCardEl.classList.contains("selectedNote")) return;
 
-  const noteEntrysElements = document.querySelectorAll(".noteCard");
-
-  noteEntrysElements.forEach((noteEntry) => {
-    noteEntry.classList.remove("selectedNote");
-  });
+  removeSelectedClassFromAllNotes();
 
   selectedCardEl.classList.add("selectedNote");
 
-  //
   const selectedCard = cardNotes.find((card) => card.id == Number(id));
 
   if (!selectedCard) return;
 
-  getInputHeader().value = selectedCard.header;
-  getInputTextArea().value = selectedCard.text;
-  // currentSelectedCardID = selectedCard.id;
+  inputHeaderElement.value = selectedCard.header;
+  inputTextAreaElement.value = selectedCard.text;
+  currentSelectedCardID = selectedCard.id;
 }
 
 //delete the selected card from DOM and Array "cardNotes"
 function deleteSelectedCard() {
+  if (!currentSelectedCardID) return;
+
   document.getElementById(currentSelectedCardID).remove();
 
   const cardIndex = cardNotes.findIndex(
@@ -129,5 +177,13 @@ function deleteSelectedCard() {
     cardNotes.splice(cardIndex, 1);
   }
   saveCardsToLocalStorage();
-  createNewNote();
+  clickNewNote();
+}
+
+function removeSelectedClassFromAllNotes() {
+  const noteCards = document.querySelectorAll(".noteCard");
+
+  noteCards.forEach((noteCard) => {
+    noteCard.classList.remove("selectedNote");
+  });
 }
